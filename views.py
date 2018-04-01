7f997beaee5d5c7d1004f3bbf52f0f7efdfa5098
Sylvain Le Bon <sylvain@happy-dev.fr>
2018-04-01 23:54:18 +0200

    feature: nested_field argument for nested container (fix #13)

--- views.py
+++ views.py
@@ -29,6 +29,13 @@ class NoCSRFAuthentication(SessionAuthentication):
 class LDPViewSet(ModelViewSet):
     model = None
     fields = None
+    exclude = None
+    parent_model = None
+    nested_field = None
+    nested_lookup_field = None #usefull?
+    nested_related_name = None
+    list_actions = {'get': 'list', 'post': 'create'}
+    detail_actions = {'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}
     renderer_classes = (JSONLDRenderer, )
     parser_classes = (JSONLDParser, )
     authentication_classes = (NoCSRFAuthentication,)
@@ -39,11 +46,22 @@ class LDPViewSet(ModelViewSet):
         if self.fields:
             meta_args['fields'] = self.fields
         else:
-            meta_args['exclude'] = ()
+            meta_args['exclude'] = self.exclude or ()
         meta_class = type('Meta', (), meta_args)
+        print('model', self.model)
         self.serializer_class = type(LDPSerializer)(self.model._meta.object_name.lower()+'Serializer', (LDPSerializer,), {'Meta': meta_class})
     
+    def get_parent(self):
+        return self.parent_model.objects.get(id=self.kwargs[self.lookup_field])
+    
+    def perform_create(self, serializer):
+        if self.parent_model:
+            serializer.validated_data[self.nested_related_name] = self.get_parent()
+        return super().perform_create(serializer)
+    
     def get_queryset(self, *args, **kwargs):
+        if self.parent_model:
+            return getattr(self.get_parent(), self.nested_field).all()
         if self.model:
             return self.model.objects.all()
         else:
@@ -59,20 +77,52 @@ class LDPViewSet(ModelViewSet):
         return response
     
     @classonlymethod
-    def urls(cls, **kwargs):
+    def get_model(cls, **kwargs):
         model = kwargs.get('model') or cls.model
-        lookup_field = kwargs.get('lookup_field') or cls.lookup_field
         if isinstance(model, str):
             model = apps.get_model(model)
-            kwargs['model'] = model
-        model_name = model._meta.object_name.lower()
+        return model
+    
+    @classonlymethod
+    def get_detail_url(cls, lookup_field=None, base_url='', **kwargs):
+        lookup_field = lookup_field or kwargs.get('lookup_field') or cls.lookup_field
+        if lookup_field and lookup_field != 'pk':
+            return r'{}(?P<{}>[\w-]+)/'.format(base_url, lookup_field)
+        return  r'{}(?P<pk>\d+)/'.format(base_url)
+    
+    @classonlymethod
+    def get_nested_urls(cls, detail_url, model_name, **kwargs):
+        nested_field = kwargs.get('nested_field') or cls.nested_field
+        if not nested_field:
+            return []
         
-        detail_url = r'^(?P<pk>\d+)$'
-        if lookup_field:
-            detail_url = r'^(?P<{}>[\w-]+)$'.format(lookup_field)
+        base_url = r'{}{}/'.format(detail_url, nested_field)
+        related_field = kwargs['model']._meta.get_field(nested_field)
+        related_name = related_field.related_query_name()
+        related_model = related_field.related_model
+        lookup_field = model_name+'_id'
         
-        urls = [
-            url(r'^$', cls.as_view({'get': 'list', 'post': 'create'}, **kwargs), name='{}-list'.format(model_name)),
-            url(detail_url, cls.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}, **kwargs), name='{}-detail'.format(model_name)),
+        nested_args = {
+             'model': related_model,
+             'exclude': (), #exclude related_name for foreignkey attributes 
+             'parent_model': kwargs['model'],
+             'nested_field': nested_field,
+             'nested_lookup_field': lookup_field,
+             'nested_related_name': related_name
+        }
+        nested_detail_url = cls.get_detail_url(lookup_field, base_url)
+        return [
+            url(base_url+'$', cls.as_view(cls.list_actions, **nested_args), name='{}-{}-list'.format(model_name, nested_field)),
+            url(nested_detail_url+'$', cls.as_view(cls.detail_actions, **nested_args), name='{}-{}-detail'.format(model_name, nested_field)),
         ]
-        return include(urls)
+    
+    @classonlymethod
+    def urls(cls, **kwargs):
+        kwargs['model'] = cls.get_model(**kwargs)
+        model_name = kwargs['model']._meta.object_name.lower()
+        detail_url = cls.get_detail_url(**kwargs)
+        
+        return include([
+            url(r'^$', cls.as_view(cls.list_actions, **kwargs), name='{}-list'.format(model_name)),
+            url(detail_url+'$', cls.as_view(cls.detail_actions, **kwargs), name='{}-detail'.format(model_name)),
+        ] + cls.get_nested_urls(detail_url, model_name, **kwargs))
