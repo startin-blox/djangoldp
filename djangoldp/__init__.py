933306383d933ea71c0053fbc9b8308316c51546
plup <plup@plup.io>
2019-01-09 01:08:36 +0100

    change: added semantic release configuration

--- djangoldp/__init__.py
+++ djangoldp/__init__.py
@@ -1,3 +1,4 @@
 from django.db.models import options
 
+__version__ = '0.0.0'
 options.DEFAULT_NAMES += ('rdf_type', 'auto_author')
--- setup.py
+++ setup.py
@@ -1,20 +1,5 @@
+#!/usr/bin/env python
+
 from setuptools import setup
 
-setup(
-    name='djangoldp',
-    version='0.5a12',
-    url='https://git.happy-dev.fr/happy-dev/djangoldp/',
-    author="Startin'blox",
-    author_email='sylvain@happy-dev.fr',
-    description='Linked Data Platform interface for Django Rest Framework',
-    packages=['djangoldp', 'djangoldp.migrations'],
-    zip_safe=False,
-    platforms='any',
-    license='MIT',
-    install_requires=[
-        'django~=1.11',
-        'django_rest_framework',
-        'pyld',
-        'django-guardian',
-    ],
-)
+setup()
