545788601eade5d2030645e66c709bb09f7fc05b
Jean-Baptiste <bleme@pm.me>
2019-01-28 11:44:21 +0100

    bugfix: force pypi update

--- djangoldp/factories.py
+++ djangoldp/factories.py
@@ -2,6 +2,7 @@ import factory
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+
 @factory.django.mute_signals(post_save)
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
