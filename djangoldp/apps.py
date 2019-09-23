399b5d70c5790a7b910e6401348580de3f61155c
Jean-Baptiste <bleme@pm.me>
2019-09-23 11:56:05 +0200

    update: auto create local source if no source exists for this type

--- djangoldp/apps.py
+++ djangoldp/apps.py
@@ -3,3 +3,21 @@ from django.apps import AppConfig
 
 class DjangoldpConfig(AppConfig):
     name = 'djangoldp'
+
+    def ready(self):
+        self.create_local_source()
+
+    def create_local_source(self):
+        from djangoldp.models import LDPSource, Model
+
+        model_classes = {cls.__name__: cls for cls in Model.__subclasses__()}
+
+        for class_name in model_classes:
+            model_class = model_classes[class_name]
+            if model_class is LDPSource:
+                continue
+            path = model_class.get_container_path().strip("/")
+            try:
+                existing_source = LDPSource.objects.get(federation=path)
+            except LDPSource.DoesNotExist:
+                LDPSource.objects.create(federation=path, urlid=Model.absolute_url(model_class))
