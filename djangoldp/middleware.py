011d280ad717e27fdba29049daeb086ac351ded9
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2019-10-22 13:45:10 +0200

    fix: redirect only on 200

--- djangoldp/middleware.py
+++ djangoldp/middleware.py
@@ -9,7 +9,7 @@ class AllowOnlySiteUrl:
 
     def __call__(self, request):
         response = self.get_response(request)
-        if(is_safe_url(request.get_raw_uri(), allowed_hosts=settings.SITE_URL)):
+        if(is_safe_url(request.get_raw_uri(), allowed_hosts=settings.SITE_URL) or response.status_code != 200):
             return response
         else:
             return redirect('{}{}'.format(settings.SITE_URL, request.path))
