6b787425a54047dcfc27d84023e6c9e54e85c900
plup <plup@plup.io>
2020-03-29 15:07:26 +0200

    feature: moved install command to cli and added configure command in management

---
+++ djangoldp/management/commands/configure.py
@@ -0,0 +1,38 @@
+from django.core import management
+from django.conf import settings
+from django.db.utils import IntegrityError
+from django.core.management.base import BaseCommand, CommandError
+from django.core.exceptions import ValidationError
+
+class Command(BaseCommand):
+
+    help = 'Install server dependencies'
+
+    def handle(self, *args, **options):
+
+        """Wrapper command around default django initialization commands."""
+
+        try:
+            # migrate data
+            management.call_command('migrate', interactive=False)
+
+        except CommandError as e:
+            setf.stdout.write(self.style.ERROR(f'Data migration failed: {e}'))
+
+        try:
+            if settings.DEBUG:
+                # create a default super user
+                from django.contrib.auth import get_user_model
+                User = get_user_model()
+                User.objects.create_superuser('admin', 'admin@example.org', 'admin')
+
+            else:
+                # call default createsuperuser command
+                management.call_command('createsuperuser', interactive=True)
+
+        except (ValidationError, IntegrityError):
+            self.stdout.write('User "admin" already exists. Skipping...')
+            pass
+
+        except CommandError as e:
+            setf.stdout.write(self.style.ERROR(f'Superuser creation failed: {e}'))
