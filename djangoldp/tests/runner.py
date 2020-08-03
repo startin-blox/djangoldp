9facf18726ce2d1635448d488e1d7fb1cfd53ee4
Calum Mackervoy <c.mackervoy@gmail.com>
2020-08-03 18:01:11 +0200

    update: performance tests separated into their own runner

--- djangoldp/tests/runner.py
+++ djangoldp/tests/runner.py
@@ -25,7 +25,7 @@ failures = test_runner.run_tests([
     'djangoldp.tests.tests_pagination',
     'djangoldp.tests.tests_inbox',
     'djangoldp.tests.tests_backlinks_service',
-    # 'djangoldp.tests.tests_temp'
+    #'djangoldp.tests.tests_temp'
 ])
 if failures:
     sys.exit(failures)
