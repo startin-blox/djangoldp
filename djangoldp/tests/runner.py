a4c4621dd64de7d95e1b8edeb9b2984363970dcb
plup <plup@plup.io>
2020-09-20 11:10:40 +0200

    feature: moved yaml settings to default settings

--- djangoldp/tests/runner.py
+++ djangoldp/tests/runner.py
@@ -4,7 +4,7 @@ import yaml
 import django
 from django.conf import settings as django_settings
 from djangoldp.conf.settings import LDPSettings
-from djangoldp.tests.settings_yaml import yaml_config
+from djangoldp.tests.settings_default import yaml_config
 
 # override config loading
 ldpsettings = LDPSettings("")
--- djangoldp/tests/settings_default.py
+++ djangoldp/tests/settings_default.py
@@ -1,94 +1,53 @@
+"""This module contains YAML configurations for djangoldp testing."""
 
-from django.conf.global_settings import *
+yaml_config = """
+dependencies:
 
-# defines default settings for testing DjangoLDP. You can use this in your own packages by following the example in
-# runner.py
-DEBUG=False
+ldppackages:
+  - djangoldp.tests
 
-ALLOWED_HOSTS=["*"]
-SITE_URL='http://happy-dev.fr'
-BASE_URL='http://happy-dev.fr'
-
-DJANGOLDP_PACKAGES=['djangoldp.tests']
-INSTALLED_APPS=('django.contrib.auth',
-               'django.contrib.contenttypes',
-               'django.contrib.sessions',
-               'django.contrib.admin',
-               'django.contrib.messages',
-               'django.contrib.staticfiles',
-               'guardian',
-               'djangoldp',
-               'djangoldp.tests',
-               )
-
-DATABASES={
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-   }
-}
-
-REST_FRAMEWORK = {
-   'DEFAULT_PAGINATION_CLASS': 'djangoldp.pagination.LDPPagination',
-   'PAGE_SIZE': 5
-}
-
-AUTH_USER_MODEL='tests.User'
-ANONYMOUS_USER_NAME = None
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-AUTHENTICATION_BACKENDS=(
-   'django.contrib.auth.backends.ModelBackend', 'guardian.backends.ObjectPermissionBackend')
-
-ROOT_URLCONF='djangoldp.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-LDP_RDF_CONTEXT={
-   "@context": {
-       "@vocab": "http://happy-dev.fr/owl/#",
-       "foaf": "http://xmlns.com/foaf/0.1/",
-       "doap": "http://usefulinc.com/ns/doap#",
-       "ldp": "http://www.w3.org/ns/ldp#",
-       "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-       "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-       "xsd": "http://www.w3.org/2001/XMLSchema#",
-       "geo": "http://www.w3.org/2003/01/geo/wgs84_pos#",
-       "acl": "http://www.w3.org/ns/auth/acl#",
-       "name": "rdfs:label",
-       "website": "foaf:homepage",
-       "deadline": "xsd:dateTime",
-       "lat": "geo:lat",
-       "lng": "geo:long",
-       "jabberID": "foaf:jabberID",
-       "permissions": "acl:accessControl",
-       "mode": "acl:mode",
-       "view": "acl:Read",
-       "change": "acl:Write",
-       "add": "acl:Append",
-       "delete": "acl:Delete",
-       "control": "acl:Control"
-   }
-}
-SEND_BACKLINKS=False
+server:
+  ALLOWED_HOSTS:
+    - '*'
+  LDP_RDF_CONTEXT:
+    "@context":
+      "@vocab": "http://happy-dev.fr/owl/#"
+      "foaf": "http://xmlns.com/foaf/0.1/"
+      "doap": "http://usefulinc.com/ns/doap#"
+      "ldp": "http://www.w3.org/ns/ldp#"
+      "rdfs": "http://www.w3.org/2000/01/rdf-schema#"
+      "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+      "xsd": "http://www.w3.org/2001/XMLSchema#"
+      "geo": "http://www.w3.org/2003/01/geo/wgs84_pos#"
+      "acl": "http://www.w3.org/ns/auth/acl#"
+      "name": "rdfs:label"
+      "website": "foaf:homepage"
+      "deadline": "xsd:dateTime"
+      "lat": "geo:lat"
+      "lng": "geo:long"
+      "jabberID": "foaf:jabberID"
+      "permissions": "acl:accessControl"
+      "mode": "acl:mode"
+      "view": "acl:Read"
+      "change": "acl:Write"
+      "add": "acl:Append"
+      "delete": "acl:Delete"
+      "control": "acl:Control"
+  AUTH_USER_MODEL: 'tests.User'
+  EMAIL_HOST: somewhere
+  ANONYMOUS_USER_NAME: None
+  AUTHENTICATION_BACKENDS:
+    - django.contrib.auth.backends.ModelBackend
+    - guardian.backends.ObjectPermissionBackend
+  ROOT_URLCONF: djangoldp.urls
+  SEND_BACKLINKS: false
+  SITE_URL: http://happy-dev.fr
+  BASE_URL: http://happy-dev.fr
+  REST_FRAMEWORK:
+    DEFAULT_PAGINATION_CLASS: djangoldp.pagination.LDPPagination
+    PAGE_SIZE: 5
+  USE_ETAGS: true
+  DEFAULT_CONTENT_TYPE: text/html
+  FILE_CHARSET: utf-8
+  SEND_BACKLINKS: False
+"""
