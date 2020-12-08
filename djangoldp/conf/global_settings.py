2f1730b0894a42e8e3eb01d37e14a8e3686a7e4c
plup <plup@plup.io>
2020-12-08 19:22:28 +0100

    feat: updated rdf context

--- djangoldp/conf/global_settings.py
+++ djangoldp/conf/global_settings.py
@@ -9,32 +9,7 @@ def gettext_noop(s):
 # LDP              #
 ####################
 
-LDP_RDF_CONTEXT = {
-    "@context": {
-      "@vocab": "http://happy-dev.fr/owl/#",
-      "foaf": "http://xmlns.com/foaf/0.1/",
-      "doap": "http://usefulinc.com/ns/doap#",
-      "ldp": "http://www.w3.org/ns/ldp#",
-      "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-      "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-      "xsd": "http://www.w3.org/2001/XMLSchema#",
-      "geo": "http://www.w3.org/2003/01/geo/wgs84_pos#",
-      "acl": "http://www.w3.org/ns/auth/acl#",
-      "name": "rdfs:label",
-      "website": "foaf:homepage",
-      "deadline": "xsd:dateTime",
-      "lat": "geo:lat",
-      "lng": "geo:long",
-      "jabberID": "foaf:jabberID",
-      "permissions": "acl:accessControl",
-      "mode": "acl:mode",
-      "view": "acl:Read",
-      "change": "acl:Write",
-      "add": "acl:Append",
-      "delete": "acl:Delete",
-      "control": "acl:Control"
-    }
-}
+LDP_RDF_CONTEXT = 'https://cdn.happy-dev.fr/owl/hdcontext.jsonld'
 
 MAX_ACTIVITY_RESCHEDULES = 3
 DEFAULT_BACKOFF_FACTOR = 1
