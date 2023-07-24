56f6fca43f2451e1c888f94a35d0a476ec1ff75b
Sylvain Le Bon <sylvain@startinblox.com>
2023-07-24 13:52:20 +0200

    update: LDPPagination is the new default

--- djangoldp/conf/default_settings.py
+++ djangoldp/conf/default_settings.py
@@ -127,7 +127,7 @@ MIDDLEWARE = [
 ##################
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_PAGINATION_CLASS': 'djangoldp.pagination.LDPOffsetPagination'
+    'DEFAULT_PAGINATION_CLASS': 'djangoldp.pagination.LDPPagination'
 }
 
 ###################
