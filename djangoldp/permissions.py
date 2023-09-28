9427eac6aa6267d5765f4fe7011f35436d5cccf9
Sylvain Le Bon <sylvain@startinblox.com>
2023-09-28 17:54:37 +0200

    update: no permission check on OPTIONS

--- djangoldp/permissions.py
+++ djangoldp/permissions.py
@@ -36,7 +36,7 @@ def join_filter_backends(*permissions, model, union=False):
 
 permission_map ={
     'GET': ['%(app_label)s.view_%(model_name)s'],
-    'OPTIONS': ['%(app_label)s.view_%(model_name)s'],
+    'OPTIONS': [],
     'HEAD': ['%(app_label)s.view_%(model_name)s'],
     'POST': ['%(app_label)s.add_%(model_name)s'],
     'PUT': ['%(app_label)s.change_%(model_name)s'],
