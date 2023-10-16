e46dfc3777ff844757f7d28a5ab2fcbf8ed9a506
Sylvain Le Bon <sylvain@startinblox.com>
2023-10-16 11:35:00 +0200

    update: context for foaf:member

--- djangoldp/models.py
+++ djangoldp/models.py
@@ -23,6 +23,7 @@ logger = logging.getLogger('djangoldp')
 
 Group._meta.serializer_fields = ['name', 'user_set']
 Group._meta.rdf_type = 'foaf:Group'
+Group._meta.rdf_context = {'user_set': 'foaf:member'}
 Group._meta.permission_classes = [OwnerPermissions, InheritPermissions]
 Group._meta.owner_field = 'user'
 Group._meta.inherit_permissions = []
