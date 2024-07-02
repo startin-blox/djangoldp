03f404952d5adc52bf20ef39bcb9628af809d23e
autodeploy <benoit@startinblox.com>
2024-07-02 17:50:48 +0200

    removing: empty space

--- djangoldp/permissions.py
+++ djangoldp/permissions.py
@@ -242,7 +242,7 @@ class IPOpenPermissions(LDPBasePermission):
 
     def has_object_permission(self, request, view, obj):
         return check_client_ip(request)
-    
+
     def get_permissions(self, user, model, obj=None):
         #Will always say there is no migrations, not taking the IP into accounts
         return set()
