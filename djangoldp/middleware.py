4de7286457f6edb9253f3b626b88e5fb2082eb48
Sylvain Roquebert <roquebertsylvain@gmail.com>
2025-04-24 15:46:48 +0200

    feature: add OptionsResponseMiddleware for handling OPTIONS requests

--- djangoldp/middleware.py
+++ djangoldp/middleware.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.utils.http import url_has_allowed_host_and_scheme
 from django.shortcuts import redirect
 from djangoldp.models import Model
-
+from django.http import HttpResponse
 
 class AllowOnlySiteUrl:
     def __init__(self, get_response):
@@ -33,3 +33,27 @@ class AllowRequestedCORSMiddleware:
         response["Access-Control-Allow-Credentials"] = 'true'
 
         return response
+
+
+class OptionsResponseMiddleware:
+    """Middleware that returns a 200 response for OPTIONS requests.
+    
+    This should be placed BEFORE the AllowRequestedCORSMiddleware in the stack
+    to ensure OPTIONS requests receive proper CORS headers.
+    
+    This middleware is not enabled by default. To use it, add
+    'djangoldp.middleware.OptionsResponseMiddleware' to your MIDDLEWARE setting
+    before 'djangoldp.middleware.AllowRequestedCORSMiddleware'.
+    """
+    
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.method == 'OPTIONS':
+            # Return an empty 200 response for OPTIONS requests
+            # The CORS headers will be added by AllowRequestedCORSMiddleware later
+            return HttpResponse(status=200)
+        
+        # For all other requests, proceed as normal
+        return self.get_response(request)
